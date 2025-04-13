ca165d7f57d9ee8820f00166ef9aea57b1b0870b
danielgao-1 <www.dgao13@gmail.com>
2025-04-13 15:01:58 -0400

    added automation for every hour

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const express = require("express"); // express backend
 const axios = require("axios"); // get request
 const cors = require("cors"); // front-end fetch  
+const cron = require("node-cron"); //automate task
 const mongoose = require("mongoose"); // for MongoDB 
 
  //initalize
@@ -39,7 +40,7 @@ const SubredditDB = mongoose.model("Subreddit", SubredditSchema);
 // api get 1
 app.get("/api/subreddit_top", async (req, res) => {
   try {
-    const response1 = await axios.get(`https://www.reddit.com/subreddits/popular.json?limit=100`); // 100 limit
+    const response1 = await axios.get(`https://www.reddit.com/subreddits/popular.json?limit=50`); // 100 limit
     const subreddits1 = response1.data.data.children.map((sub) => ({
       name: sub.data.display_name,
       description: sub.data.public_description || "No description", // not used for frontend
@@ -47,10 +48,10 @@ app.get("/api/subreddit_top", async (req, res) => {
       url: `https://www.reddit.com/r/${sub.data.display_name}`,
     }));
 
-    await sleep(1000); // 1 sec delay
+    await sleep(2000); // 1 sec delay
     // run twice  
     const after = response1.data.data.after;
-    const response2 = await axios.get(`https://www.reddit.com/subreddits/popular.json?limit=100&after=${after}`); // another 100 subreddits
+    const response2 = await axios.get(`https://www.reddit.com/subreddits/popular.json?limit=50&after=${after}`); // another 100 subreddits
     const subreddits2 = response2.data.data.children.map((sub) => ({
       name: sub.data.display_name,
       description: sub.data.public_description || "No description", // not used for frontend
@@ -87,22 +88,35 @@ app.get("/api/subreddit_stats", async (req, res) => {
   try {
     const storedSubreddits = await SubredditDB.find()
     .sort({subscribers: -1})
-    .limit(25); // Get 5 stored subreddits. Increase limit if needed
+    .skip(0)
+    .limit(100); // Get 25 stored subreddits. Increase limit if needed
     const subredditNames = storedSubreddits.map(sub => sub.name); // 
 
     for (const subreddit of subredditNames) {
-      await sleep(100); // delay
+      await sleep(1800); // delay
 
       try {
         console.log(`Fetching posts for r/${subreddit}...`); // log to show subreddit
 
-        const postsResponse = await axios.get(`https://www.reddit.com/r/${subreddit}/new.json?limit=100`);
-        const posts = postsResponse.data.data.children.map(post => ({
+        const postsResponse1 = await axios.get(`https://www.reddit.com/r/${subreddit}/new.json?limit=100`);
+        const posts = postsResponse1.data.data.children.map(post => ({
           created_utc: post.data.created_utc,
           num_comments: post.data.num_comments,
           score: post.data.score || 0,
           upvote_ratio: post.data.upvote_ratio || null,
         }));
+
+        /*
+        // grabs the next too response based on thhe # of subreddits 
+        const after = postsResponse1.data.data.after;
+        const postsResponse2 = await axios.get(`https://www.reddit.com/r/${subreddit}/new.json?limit=100&after=${after}`);
+        const posts2 = postsResponse2.data.data.children.map(post => ({
+          created_utc: post.data.created_utc,
+          num_comments: post.data.num_comments,
+          score: post.data.score || 0,
+          upvote_ratio: post.data.upvote_ratio || null,
+        }));
+        */
 
         const now = Math.floor(Date.now() / 1000);
         let count_post = 0;
@@ -113,14 +127,14 @@ app.get("/api/subreddit_stats", async (req, res) => {
         let total_upvote_ratio = 0;
         let total_comments = 0;
         let total_score = 0;
-        
+
         posts.forEach(post => {
           const postTime = post.created_utc;
 
-          if (now - postTime <= 14400) posts_4hours++;
-          if (now - postTime <= 86400) posts_24hours++;
-          if (now - postTime <= 172800) posts_48hours++;
-          if (now - postTime <= 604800) posts_7days++;
+          if (now - postTime <= 3600) posts_4hours++;
+          if (now - postTime <= 7200) posts_24hours++;
+          if (now - postTime <= 43200) posts_48hours++;
+          if (now - postTime <= 86400) posts_7days++;
           count_post += 1;
           total_comments += post.num_comments;
           total_score += post.score;
@@ -143,7 +157,7 @@ app.get("/api/subreddit_stats", async (req, res) => {
             }
           }
         );
-        res.json()
+
       } catch (error) {
         console.error(error.message);
       }
@@ -162,6 +176,18 @@ app.get("/api/tofrontend", async (req, res) => {
   }
   catch (error) {
     console.error(error.message)
+  }
+});
+
+// run every hour
+cron.schedule("0 * * * *", async () => {
+  try {
+    await axios.get("http://localhost:3000/api/subreddit_top");
+    await sleep(1000)
+    await axios.get("http://localhost:3000/api/subreddit_stats");
+   
+  } catch (error) {
+    console.error(error.message);
   }
 });
 
