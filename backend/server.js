624593b56b2c7f4ab396482be99d99a2408ac5db
danielgao-1 <www.dgao13@gmail.com>
2025-04-19 14:33:05 -0400

    backend mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const express = require("express"); // express backend
 const axios = require("axios"); // get request
 const cors = require("cors"); // front-end fetch  
 const cron = require("node-cron"); //automate task
 const mongoose = require("mongoose"); // for MongoDB 
-require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
  //initalize
 const app = express();
@@ -16,12 +17,39 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms || DEF_DELAY));
 }
 
-// mongoose connect
-mongoose.connect(process.env.MONGO_URI, {
+
+
+const uri = process.env.MONGO_URI;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-.then(() => console.log("Connected to MongoDB"));
+});
+
 
 // mongodb schema
 const SubredditSchema = new mongoose.Schema({
